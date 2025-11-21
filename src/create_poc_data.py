ec2dede533017a906a669f1100b6e9f84f17ecf3
miles-lenz <miles@lenz-be.de>
2025-11-21 16:24:19 +0100

    Enhance poc_data script to remove scale and ID from SVG images

--- src/create_poc_data.py
+++ src/create_poc_data.py
@@ -1,12 +1,29 @@
 """Create a subset of the original data to use for a PoC."""
 
+import xml.etree.ElementTree as ET
 import shutil
 from pathlib import Path
 import pandas as pd
 
 
-IMG_FORMAT = "png"  # either 'png' or 'svg'
+IMG_FORMAT = "svg"  # either 'png' or 'svg'
+SVG_CLEAN = True  # removes ID and scale
+SVG_NS = "{http://www.w3.org/2000/svg}"  # namespace
+
 TYPES = ["Drag. 32", "Drag. 33", "Drag. 37", "Drag. 18/31"]
+
+
+def clean_svg(src_path, dst_path):
+    """Removes ID and scale from the given SVG image."""
+
+    tree = ET.parse(src_path)
+    root = tree.getroot()
+
+    for child in list(root):
+        if child.tag in [f"{SVG_NS}text", f"{SVG_NS}rect"]:
+            root.remove(child)
+
+    tree.write(dst_path)
 
 
 def main():
@@ -37,12 +54,25 @@ def main():
         dst_folder.mkdir(parents=True, exist_ok=True)
 
         ids = df[df["Typ"] == type_]["Id"]
+        print(f"[info] Processing {type_} with {len(ids)} items.")
+
         for id_ in ids:
+
             img_name = f"{id_}.{IMG_FORMAT}"
             if IMG_FORMAT == "svg":
                 img_name = "recons_" + img_name
-            img_path = Path(f"data/{IMG_FORMAT}/{img_name}")
-            shutil.copy2(img_path, dst_folder)
+
+            src_path = Path(f"data/{IMG_FORMAT}/{img_name}")
+            dst_path = dst_folder / img_name
+
+            if not src_path.exists():
+                print(f"[warn] Path '{src_path}' does not exist and will be skipped.")
+                continue
+
+            if IMG_FORMAT == "svg" and SVG_CLEAN:
+                clean_svg(src_path, dst_path)
+            else:
+                shutil.copy2(src_path, dst_folder)
 
 
 if __name__ == "__main__":
